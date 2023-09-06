29223843d01267289d789b7e254507341cf0994a
Jorge Saldana <jojonoob134@gmail.com>
2023-09-06 14:45:29 -0500

    finished exercise 1

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -18,32 +18,57 @@ namespace ChallengesWithTestsMark8
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            
+            double answer = minuend - subtrahend;
+            return answer;
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            int answer = number1 + number2;
+            return answer;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+           
+            if (number1 < number2)
+            {
+                return number1;
+            }
+            else
+            {
+                return number2;
+            }
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            
+            long answer = factor1 * factor2;
+            return answer;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            string answer;
+            if (nameOfPerson == "")
+            {
+                answer = "Hello!";
+            }
+            else
+            {
+                answer = ($"Hello, {nameOfPerson}!");
+            }
+            
+            return answer;
         }
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            
+            string hello = "HEY!";
+            return hello;
         }
     }
 }
